df3121f740526a5d3d2778d8739f1b3426250f3c
User Martymac <martymac@laptop.martymac.org>
2012-02-22 18:00:26 +0100

    Leave 3 tests

--- file_memory_test.c
+++ file_memory_test.c
@@ -1,9 +1,11 @@
-#define DEBUG                   1
-#define WITH_FILE_MEMORY        1
 #define FILE_MEMORY_CHUNK_SIZE  (32 * 1024 * 1024) /* 32 MB */
 #define MAX_FILE_MEMORY_CHUNKS  0
 
 #include "file_memory.h"
+
+/* XXX defined in file_memory.c */
+#define FILE_MALLOC_HEADER_SIZE \
+    (sizeof(struct file_memory*))
 
 #include <stdio.h>
 
@@ -11,56 +13,44 @@ int main(void)
 {
     void *ptr = NULL;
 
+    /* init memory manager */
     if(init_memory("/tmp/.fpart_mem", MAX_FILE_MEMORY_CHUNKS) != 0) {
         fprintf(stderr, "%s(): could not initialize memory manager\n",
             __func__);
         return (1);
     }
 
-    fprintf(stderr, "allocating FILE_MEMORY_CHUNK_SIZE bytes (should fill a full file_memory)\n");
-    if((ptr = fmalloc(FILE_MEMORY_CHUNK_SIZE)) == NULL) {
-        fprintf(stderr, "could not allocate FILE_MEMORY_CHUNK_SIZE bytes\n");
+    fprintf(stderr, "allocating (FILE_MEMORY_CHUNK_SIZE * 3 - FILE_MALLOC_HEADER_SIZE) bytes (should create a new file_memory of FILE_MEMORY_CHUNK_SIZE * 3 bytes)\n");
+    if((ptr = fmalloc(FILE_MEMORY_CHUNK_SIZE * 3 - FILE_MALLOC_HEADER_SIZE)) == NULL) {
+        fprintf(stderr, "could not allocate (FILE_MEMORY_CHUNK_SIZE * 3) bytes\n");
     }
-    else
+    else {
+        fprintf(stderr, "==\n");
         ffree(ptr);
-    fprintf(stderr, "==\n");
-
-    fprintf(stderr, "allocating FILE_MEMORY_CHUNK_SIZE bytes (should fill a full file_memory)\n");
-    if(fmalloc(FILE_MEMORY_CHUNK_SIZE) == NULL) {
-        fprintf(stderr, "could not allocate FILE_MEMORY_CHUNK_SIZE bytes\n");
     }
     fprintf(stderr, "==\n");
 
-    fprintf(stderr, "allocating 1 byte (should create a new file_memory)\n");
-    if(fmalloc(1) == NULL) {
-        fprintf(stderr, "could not allocate 1 byte\n");
-    }
-    fprintf(stderr, "==\n");
-
-    fprintf(stderr, "allocating 1 byte (should continue within the same file_memory)\n");
-    if(fmalloc(1) == NULL) {
-        fprintf(stderr, "could not allocate 1 byte\n");
-    }
-    fprintf(stderr, "==\n");
-
-    fprintf(stderr, "allocating 8 bytes (should continue within the same file_memory)\n");
-    if(fmalloc(8) == NULL) {
-        fprintf(stderr, "could not allocate 8 bytes\n");
-    }
-    fprintf(stderr, "==\n");
-
-    fprintf(stderr, "allocating FILE_MEMORY_CHUNK_SIZE bytes (should create a new file_memory)\n");
-    if(fmalloc(FILE_MEMORY_CHUNK_SIZE) == NULL) {
-        fprintf(stderr, "could not allocate FILE_MEMORY_CHUNK_SIZE bytes\n");
-    }
-    fprintf(stderr, "==\n");
-
-    fprintf(stderr, "allocating (FILE_MEMORY_CHUNK_SIZE * 3 + 1) bytes (should create a new file_memory of FILE_MEMORY_CHUNK_SIZE * 4 bytes)\n");
-    if(fmalloc(FILE_MEMORY_CHUNK_SIZE * 3 + 1) == NULL) {
+    fprintf(stderr, "allocating (FILE_MEMORY_CHUNK_SIZE * 3 - FILE_MALLOC_HEADER_SIZE + 1) bytes (should create a new file_memory of FILE_MEMORY_CHUNK_SIZE * 4 bytes)\n");
+    if((ptr = fmalloc(FILE_MEMORY_CHUNK_SIZE * 3 - FILE_MALLOC_HEADER_SIZE + 1)) == NULL) {
         fprintf(stderr, "could not allocate (FILE_MEMORY_CHUNK_SIZE * 3 + 1) bytes\n");
     }
+    else {
+        fprintf(stderr, "==\n");
+        ffree(ptr);
+    }
     fprintf(stderr, "==\n");
 
+    fprintf(stderr, "allocating (FILE_MEMORY_CHUNK_SIZE * 3 - FILE_MALLOC_HEADER_SIZE + 1) bytes (should create a new file_memory of FILE_MEMORY_CHUNK_SIZE * 4 bytes)\n");
+    if((ptr = fmalloc(FILE_MEMORY_CHUNK_SIZE * 3 - FILE_MALLOC_HEADER_SIZE + 1)) == NULL) {
+        fprintf(stderr, "could not allocate (FILE_MEMORY_CHUNK_SIZE * 3 + 1) bytes\n");
+    }
+    else {
+        fprintf(stderr, "==\n");
+        ffree(ptr);
+    }
+    fprintf(stderr, "==\n");
+
+    /* uninit memory manager */
     uninit_memory();
     return (0);
 }
