martymac/fpart

commit 3235425d634ba3c1d61df14117c1bac9bebc70b4

fpart: accept human-friendly size formats for options -s, -p, -q and -r

src/utils.c
@@ -70,9 +70,45 @@
 /* isblank(3) */
 #include <ctype.h>
 
+/* strtoumax(3) */
+#include <limits.h>
+#include <inttypes.h>
+
 /****************
  Helper functions
  ****************/
+
+/* Convert a char (K, M, G, ...) to a size multiplier */
+uintmax_t
+char_to_multiplier(const char c)
+{
+    uintmax_t ret = 0;
+
+    switch(c) {
+        case 'k':
+        case 'K':
+            ret = 1 << 10;
+            break;
+        case 'm':
+        case 'M':
+            ret = 1 << 20;
+            break;
+        case 'g':
+        case 'G':
+            ret = 1 << 30;
+            break;
+        case 't':
+        case 'T':
+            ret = (uintmax_t)1 << 40;
+            break;
+        case 'p':
+        case 'P':
+            ret = (uintmax_t)1 << 50;
+            break;
+    }
+
+    return (ret);
+}
 
 /* Return the number of digits necessary to print i */
 unsigned int
@@ -268,6 +304,60 @@ str_is_negative(const char *str)
         return (1);
     else
         return (0);
+}
+
+/* Convert a str to a uintmax > 0
+   - support human-friendly multipliers
+   - only accept values > 0 as input
+   - return 0 if an error occurs */
+uintmax_t
+str_to_uintmax(const char *str, const unsigned char handle_multiplier)
+{
+    assert(str != NULL);
+
+    char *endptr = NULL;
+    uintmax_t val = 0;
+    uintmax_t multiplier = 0;
+
+    /* check if a negative value has been provided */
+    if(str_is_negative(str))
+        return (0);
+
+    errno = 0;
+    val = strtoumax(str, &endptr, 10);
+    /* check that something was converted and refuse invalid values */
+    if((endptr == optarg) || (val == 0))
+        return (0);
+    /* check for other errors */
+    if(errno != 0) {
+        fprintf(stderr, "%s(): %s\n", __func__, strerror(errno));
+        return (0);
+    }
+    /* if characters remain, handle multiplier */
+    if(*endptr != '\0') {
+        /* return an error if we do not want to handle multiplier */
+        if(!handle_multiplier)
+            return (0);
+
+        uintmax_t orig_val = val;
+        /* more than one character remain or invalid multiplier specified */
+        if ((*(endptr + 1) != '\0') ||
+            (multiplier = char_to_multiplier(*endptr)) == 0) {
+            fprintf(stderr, "%s(): %s\n", __func__, "invalid unit provided");
+            return (0);
+        }
+        /* check for overflow */
+        val *= multiplier;
+        if((val / multiplier) != orig_val) {
+            fprintf(stderr, "%s(): %s\n", __func__, strerror(ERANGE));
+            return (0);
+        }
+    }
+#if defined(DEBUG)
+    fprintf(stderr, "%s(): converted string %s to value %ju\n", __func__,
+        optarg, val);
+#endif
+    return (val);
 }
 
 /* Match an fts entry against an array of strings

TODO
@@ -12,7 +12,6 @@ Fpart:
 - Add an option to specify that a directory matching a path or a pattern should
   not be split but treated as a file entry
 - Add constraints, e.g. : force hardlinks to belong to the same partition
-- Accept size values in a human-friendly format
 - Improve sort by using, e.g. : http://en.wikipedia.org/wiki/External_sorting
 - Display total size in final status
 - As a second pass, remove partitions with no file (e.g. option -n with too many

man/fpart.1
@@ -102,6 +102,7 @@ This option can be used in conjunction with
 .Fl f
 and
 .Fl L .
+You can use a human-friendly unit suffix here (k, m, g, t, p).
 .El
 .Sh INPUT CONTROL
 .Bl -tag -width indent
@@ -161,8 +162,8 @@ Include files or directories matching
 only (and discard all other files).
 This option may be specified several times.
 .Ar Pattern
-may be a leaf (file or directory) name or a specific path. Shell pattern
-matching characters
+may be a leaf (file or directory) name or a specific path.
+Shell pattern matching characters
 .Dq ( Li \&[ ,
 .Dq Li \&] ,
 .Dq Li * ,
@@ -191,8 +192,8 @@ and
 In this case, exclusion is performed after.
 This option may be specified several times.
 .Ar Pattern
-may be a leaf (file or directory) name or a specific path. Shell pattern
-matching characters
+may be a leaf (file or directory) name or a specific path.
+Shell pattern matching characters
 .Dq ( Li \&[ ,
 .Dq Li \&] ,
 .Dq Li * ,
@@ -312,16 +313,19 @@ when finishing a partition (after having closed last output file, if any).
 Preload each partition with
 .Ar num
 bytes.
+You can use a human-friendly unit suffix here (k, m, g, t, p).
 .It Ic -q Ar num
 Overload each file size with
 .Ar num
 bytes.
+You can use a human-friendly unit suffix here (k, m, g, t, p).
 .It Ic -r Ar num
 Round each file size up to next
 .Ar num
 bytes multiple.
 This option can be used in conjunction with overloading, which is done *before*
 rounding.
+You can use a human-friendly unit suffix here (k, m, g, t, p).
 .El
 .Sh EXAMPLES
 Here are some examples:

src/fpart.c
@@ -273,92 +273,36 @@ handle_options(struct program_options *options, int *argcp, char ***argvp)
                 return (FPART_OPTS_VERSION | FPART_OPTS_OK | FPART_OPTS_EXIT);
             case 'n':
             {
-                char *endptr = NULL;
-                uintmax_t num_parts = 0;
-                /* check if a negative value has been provided */
-                if(str_is_negative(optarg)) {
+                uintmax_t num_parts = str_to_uintmax(optarg, 0);
+                if(num_parts == 0) {
                     fprintf(stderr,
                         "Option -n requires a value greater than 0.\n");
                     return (FPART_OPTS_USAGE |
                         FPART_OPTS_NOK | FPART_OPTS_EXIT);
-                }
-                errno = 0;
-                num_parts = strtoumax(optarg, &endptr, 10);
-                /* check that something was converted, refuse
-                   partially-converted and invalid arguments */
-                if((endptr == optarg) || (*endptr != '\0') ||
-                    (num_parts == 0)) {
-                    fprintf(stderr,
-                        "Option -n requires a value greater than 0.\n");
-                    return (FPART_OPTS_USAGE |
-                        FPART_OPTS_NOK | FPART_OPTS_EXIT);
-                }
-                /* check for other errors */
-                if(errno != 0) {
-                    fprintf(stderr, "%s(): %s\n", __func__,
-                        strerror(errno));
-                    return (FPART_OPTS_NOK | FPART_OPTS_EXIT);
                 }
                 options->num_parts = (pnum_t)num_parts;
                 break;
             }
             case 'f':
             {
-                char *endptr = NULL;
-                uintmax_t max_entries = 0;
-                /* check if a negative value has been provided */
-                if(str_is_negative(optarg)) {
+                uintmax_t max_entries = str_to_uintmax(optarg, 0);
+                if(max_entries == 0) {
                     fprintf(stderr,
                         "Option -f requires a value greater than 0.\n");
                     return (FPART_OPTS_USAGE |
                         FPART_OPTS_NOK | FPART_OPTS_EXIT);
-                }
-                errno = 0;
-                max_entries = strtoumax(optarg, &endptr, 10);
-                /* check that something was converted, refuse
-                   partially-converted and invalid arguments */
-                if((endptr == optarg) || (*endptr != '\0') ||
-                    (max_entries == 0)) {
-                    fprintf(stderr,
-                        "Option -f requires a value greater than 0.\n");
-                    return (FPART_OPTS_USAGE |
-                        FPART_OPTS_NOK | FPART_OPTS_EXIT);
-                }
-                /* check for other errors */
-                if(errno != 0) {
-                    fprintf(stderr, "%s(): %s\n", __func__,
-                        strerror(errno));
-                    return (FPART_OPTS_NOK | FPART_OPTS_EXIT);
                 }
                 options->max_entries = (fnum_t)max_entries;
                 break;
             }
             case 's':
             {
-                char *endptr = NULL;
-                uintmax_t max_size = 0;
-                /* check if a negative value has been provided */
-                if(str_is_negative(optarg)) {
+                uintmax_t max_size = str_to_uintmax(optarg, 1);
+                if(max_size == 0) {
                     fprintf(stderr,
                         "Option -s requires a value greater than 0.\n");
                     return (FPART_OPTS_USAGE |
                         FPART_OPTS_NOK | FPART_OPTS_EXIT);
-                }
-                errno = 0;
-                max_size = strtoumax(optarg, &endptr, 10);
-                /* check that something was converted, refuse
-                   partially-converted and invalid arguments */
-                if((endptr == optarg) || (*endptr != '\0') || (max_size == 0)) {
-                    fprintf(stderr,
-                        "Option -s requires a value greater than 0.\n");
-                    return (FPART_OPTS_USAGE |
-                        FPART_OPTS_NOK | FPART_OPTS_EXIT);
-                }
-                /* check for other errors */
-                if(errno != 0) {
-                    fprintf(stderr, "%s(): %s\n", __func__,
-                        strerror(errno));
-                    return (FPART_OPTS_NOK | FPART_OPTS_EXIT);
                 }
                 options->max_size = (fsize_t)max_size;
                 break;
@@ -507,93 +451,36 @@ handle_options(struct program_options *options, int *argcp, char ***argvp)
             }
             case 'p':
             {
-                char *endptr = NULL;
-                uintmax_t preload_size = 0;
-                /* check if a negative value has been provided */
-                if(str_is_negative(optarg)) {
+                uintmax_t preload_size = str_to_uintmax(optarg, 1);
+                if(preload_size == 0) {
                     fprintf(stderr,
                         "Option -p requires a value greater than 0.\n");
                     return (FPART_OPTS_USAGE |
                         FPART_OPTS_NOK | FPART_OPTS_EXIT);
-                }
-                errno = 0;
-                preload_size = strtoumax(optarg, &endptr, 10);
-                /* check that something was converted, refuse
-                   partially-converted and invalid arguments */
-                if((endptr == optarg) || (*endptr != '\0') ||
-                    (preload_size == 0)) {
-                    fprintf(stderr,
-                        "Option -p requires a value greater than 0.\n");
-                    return (FPART_OPTS_USAGE |
-                        FPART_OPTS_NOK | FPART_OPTS_EXIT);
-                }
-                /* check for other errors */
-                if(errno != 0) {
-                    fprintf(stderr, "%s(): %s\n", __func__,
-                        strerror(errno));
-                    return (FPART_OPTS_NOK | FPART_OPTS_EXIT);
                 }
                 options->preload_size = (fsize_t)preload_size;
                 break;
             }
             case 'q':
             {
-                char *endptr = NULL;
-                uintmax_t overload_size = 0;
-                /* check if a negative value has been provided */
-                if(str_is_negative(optarg)) {
+                uintmax_t overload_size = str_to_uintmax(optarg, 1);
+                if(overload_size == 0) {
                     fprintf(stderr,
                         "Option -q requires a value greater than 0.\n");
                     return (FPART_OPTS_USAGE |
                         FPART_OPTS_NOK | FPART_OPTS_EXIT);
-                }
-                errno = 0;
-                overload_size = strtoumax(optarg, &endptr, 10);
-                /* check that something was converted, refuse
-                   partially-converted and invalid arguments */
-                if((endptr == optarg) || (*endptr != '\0') ||
-                    (overload_size == 0)) {
-                    fprintf(stderr,
-                        "Option -q requires a value greater than 0.\n");
-                    return (FPART_OPTS_USAGE |
-                        FPART_OPTS_NOK | FPART_OPTS_EXIT);
-                }
-                /* check for other errors */
-                if(errno != 0) {
-                    fprintf(stderr, "%s(): %s\n", __func__,
-                        strerror(errno));
-                    return (FPART_OPTS_NOK | FPART_OPTS_EXIT);
                 }
                 options->overload_size = (fsize_t)overload_size;
                 break;
             }
             case 'r':
             {
-                char *endptr = NULL;
-                uintmax_t round_size = 0;
-                /* check if a negative value has been provided */
-                if(str_is_negative(optarg)) {
-                    fprintf(stderr,
-                        "Option -r requires a value greater than 0.\n");
-                    return (FPART_OPTS_USAGE |
-                        FPART_OPTS_NOK | FPART_OPTS_EXIT);
-                }
-                errno = 0;
-                round_size = strtoumax(optarg, &endptr, 10);
-                /* check that something was converted, refuse
-                   partially-converted and invalid arguments */
-                if((endptr == optarg) || (*endptr != '\0') ||
-                    (round_size <= 1)) {
+                uintmax_t round_size = str_to_uintmax(optarg, 1);
+                if(round_size <= 1) {
                     fprintf(stderr,
                         "Option -r requires a value greater than 1.\n");
                     return (FPART_OPTS_USAGE |
                         FPART_OPTS_NOK | FPART_OPTS_EXIT);
-                }
-                /* check for other errors */
-                if(errno != 0) {
-                    fprintf(stderr, "%s(): %s\n", __func__,
-                        strerror(errno));
-                    return (FPART_OPTS_NOK | FPART_OPTS_EXIT);
                 }
                 options->round_size = (fsize_t)round_size;
                 break;

src/utils.h
@@ -79,6 +79,7 @@
         err_action                                                      \
     }
 
+uintmax_t char_to_multiplier(const char c);
 unsigned int get_num_digits(double i);
 fsize_t get_size(char *file_path, struct stat *file_stat,
     struct program_options *options);
@@ -86,6 +87,7 @@ char *abs_path(const char *path);
 int str_push(char ***array, unsigned int *num, const char * const str);
 void str_cleanup(char ***array, unsigned int *num);
 int str_is_negative(const char * const str);
+uintmax_t str_to_uintmax(const char *str, const unsigned char handle_multiplier);
 int file_match(const char * const * const array, const unsigned int num,
     const FTSENT * const p, const unsigned char ignore_case);
 int valid_file(const FTSENT * const p, struct program_options *options,

commit 8caa93b915c068e65bbac4a6475213f14e7cb199

fpart/utils.c: Fix incorrect use of optarg
Introduced with commit 3235425d when switching to str_to_uintmax().

Spotted by: Claude (Anthropic)

src/utils.c
@@ -473,7 +473,7 @@ str_to_uintmax(const char *str, const unsigned char handle_multiplier)
     errno = 0;
     val = strtoumax(str, &endptr, 10);
     /* check that something was converted and refuse invalid values */
-    if((endptr == optarg) || (val == 0))
+    if((endptr == str) || (val == 0))
         return (0);
     /* check for other errors */
     if(errno != 0) {
@@ -504,7 +504,7 @@ str_to_uintmax(const char *str, const unsigned char handle_multiplier)
     }
 #if defined(DEBUG)
     fprintf(stderr, "%s(): converted string %s to value %ju\n", __func__,
-        optarg, val);
+        str, val);
 #endif
     return (val);
 }